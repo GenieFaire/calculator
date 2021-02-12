61aad9159925ea4fadbb97f30a232a39e02148c2
GenieFaire <jennifer.cavaccuiti@yahoo.fr>
2021-02-12 15:27:44 +0100

    feat : fourth operations

--- calculatrice.js
+++ calculatrice.js
@@ -1,64 +1,59 @@
-function addition(terme1, terme2) {
-    return terme1 + terme2;
+let operatorNumber = false;
+let result = '';
+let operation = '';
+
+function getNumber(number) {
+    operation += number;
+    displayOperation();
 }
 
-function soustraction(terme1, terme2) {
-    return terme1 - terme2;
-}
-
-function multiplication(facteur1, facteur2) { 
-    return facteur1 * facteur2;
-}
-
-function division(dividende, diviseur) {
-    if (diviseur === 0) {
-        throw new Error('on ne peut pas diviser par 0 !!!!');
+function getOperator(operatorCapture) {
+    if (result !== '') {
+        operatorNumber = false;
+        operation = result;
+    }
+    if (operatorNumber === true) {
+        errorMessage("Calcul à un seul opérateur");
     } else {
-        return dividende / diviseur;
+        operatorNumber = true;
+        operation += ' ' + operatorCapture + ' ';
+    }
+    displayOperation();
+}
+
+// TODO faire passer le nombre négatif en positif
+function getRelative() {
+    if (operatorNumber === false) {
+        operation = '-' + operation;
+    } else {
+        operation = operation.slice(0, operation.lastIndexOf(' ')) + ' -' + operation.slice(operation.lastIndexOf(' ') + 1);
+    }
+    displayOperation();
+}
+
+function getResult() {
+    if (operation === '') {
+        errorMessage('Il faut saisir une opération');
+    } else {
+        result = eval(operation);
+        document.getElementById('display').innerHTML = result;
     }
 }
 
-try {
-    do {
-        var operation = Number(prompt('Quelle opération voulez-vous effectuer ? \n'
-        + '1 - Addition \n'
-        + '2 - Multiplication \n'
-        + '3 - Soustraction \n'
-        + '4 - Division'));
-    
-        console.log(operation);
-    } while(operation != 1 && operation != 2 && operation != 3 && operation != 4);
-    
-    do {
-        var firstNumber = (Number(prompt('Entrez le premier nombre : ')));
-    } while(isNaN(firstNumber));
-    
-    do {
-        var secondNumber = (Number(prompt('Entrez le deuxième nombre : ')));
-    } while(isNaN(secondNumber));
-    
-    switch (operation) {
-        case 1:
-            var result = addition(firstNumber, secondNumber);
-            break;
-        case 2:
-            var result = soustraction(firstNumber, secondNumber);
-            break;
-        case 3:
-            var result = multiplication(firstNumber, secondNumber);
-            break;
-        case 4:
-            var result = division(firstNumber, secondNumber);
-            break;
-        default:
-            throw new Error('Cette opération n\'est pas connue');
-            break;
-    }
-    
-        alert('Le résultat est : ' + result);
-        
-} catch (error) {
-    alert(error);
+function reset() {
+    operation = '';
+    operatorNumber = false;
+    result = '';
+    displayOperation();
 }
 
+function errorMessage(message) {
+    document.getElementById('display').innerHTML = message;
+    setTimeout(function () {
+        displayOperation();
+    }, 2000);
+}
 
+function displayOperation() {
+    document.getElementById('display').innerHTML = operation;
+}
