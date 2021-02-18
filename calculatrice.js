73c503f3330c61b1da7651e0cf932ba9cb8ecc32
GenieFaire <jennifer.cavaccuiti@yahoo.fr>
2021-02-18 05:44:08 +0100

    feat : last release

--- calculatrice.js
+++ calculatrice.js
@@ -1,53 +1,58 @@
-let operatorNumber = false;
-let result = '';
-let operation = '';
+let result = '', operator = '', number1 = '', number2 = '';
 
 function getNumber(number) {
-    operation += number;
+    if (result !== '') {
+        reset();
+    }
+    operator === '' ? number1 += number : number2 += number;
     displayOperation();
 }
 
 function getOperator(operatorCapture) {
-    if (operation === '') {
+    if (number1 === '') {
         errorMessage("On commence par les chiffres");
-    } else {
-        if (result !== '') {
-            operatorNumber = false;
-            operation = result;
-        }
-        if (operatorNumber === true) {
-            errorMessage("Calcul à un seul opérateur");
-        } else {
-            operatorNumber = true;
-            operation += ' ' + operatorCapture + ' ';
-        }
-        displayOperation();
     }
-
-}
-
-// TODO faire passer le nombre négatif en positif
-function getRelative() {
-    if (operatorNumber === false) {
-        operation = '-' + operation;
-    } else {
-        operation = operation.slice(0, operation.lastIndexOf(' ')) + ' -' + operation.slice(operation.lastIndexOf(' ') + 1);
+    if (result !== '') {
+        let newNumber1 = result;
+        reset();
+        number1 = newNumber1;
     }
+    operator !== '' ? errorMessage("Calcul à un seul opérateur") : operator += operatorCapture;
     displayOperation();
 }
 
+function getRelative() {
+    if (result !== '') {
+        let newNumber1 = result.toString();
+        reset();
+        number1 = newNumber1;
+        console.log(typeof number1);
+        console.log(number1.charAt(0));
+    }
+    operator === '' ? number1 = changeNumber(number1) : number2 = changeNumber(number2);
+    displayOperation();
+}
+
+function changeNumber(number) {
+    console.log(number.charAt(0));
+    return number.charAt(0) === '-' ? number.slice(1) : '-' + number;
+}
+
+
 function getResult() {
-    if (operation === '') {
+    if (operator === '') {
         errorMessage('Il faut saisir une opération');
     } else {
-        result = eval(operation);
+        result = eval(number1 + ' ' + operator + ' ' + number2);
+        number1 = result;
         document.getElementById('display').innerHTML = result;
     }
 }
 
 function reset() {
-    operation = '';
-    operatorNumber = false;
+    operator = '';
+    number1 = '';
+    number2 = '';
     result = '';
     displayOperation();
 }
@@ -60,5 +65,5 @@ function errorMessage(message) {
 }
 
 function displayOperation() {
-    document.getElementById('display').innerHTML = operation;
+    document.getElementById('display').innerHTML = number1 + ' ' + operator + ' ' + number2;
 }
